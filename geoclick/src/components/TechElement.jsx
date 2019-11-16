be5a72eade6f92e1375f543051f7882e084f5891
Guillermo Villalta <mexcelus@gmail.com>
2019-11-15 18:07:42 -0700

    new structure

--- geoclick/src/components/TechElement.jsx
+++ geoclick/src/components/TechElement.jsx
@@ -5,7 +5,7 @@ function TechElement(props) {
       <>
         <p>Tech Element!</p>
         <img
-          src="http://www.myiconfinder.com/uploads/iconsets/e42f329d0832c5a8369e6006056ad82f.png"
+          src=""
           style={{width: '100px'}}
           alt=""
           srcset=""
