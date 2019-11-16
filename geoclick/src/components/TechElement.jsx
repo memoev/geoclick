d114bd5b186a233f470754376bb287193b381ff7
Guillermo Villalta <mexcelus@gmail.com>
2019-11-15 17:23:08 -0700

    images images images

--- geoclick/src/components/TechElement.jsx
+++ geoclick/src/components/TechElement.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 function TechElement(props) {
-    return <>
+    return (
+      <>
         <p>Tech Element!</p>
-    </>
+        <img
+          src="http://www.myiconfinder.com/uploads/iconsets/e42f329d0832c5a8369e6006056ad82f.png"
+          style={{width: '100px'}}
+          alt=""
+          srcset=""
+        />
+      </>
+    );
 }
 
 export default TechElement;
