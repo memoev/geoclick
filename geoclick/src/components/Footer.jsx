59513f0ed5162ca367bbd9c08766e625bbfec050
Guillermo Villalta <mexcelus@gmail.com>
2019-11-15 23:20:46 -0700

    text

--- geoclick/src/components/Footer.jsx
+++ geoclick/src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
 function Footer() {
   return (
     <footer class="footer">
       <div class="container">
-        <span class="text-muted">⚛️ - react emoji</span>
+        <span class="text-muted">⚛️</span>
       </div>
     </footer>
   );
