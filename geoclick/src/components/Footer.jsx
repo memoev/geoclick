c54c79c8656c8aa9e3e7ce2bd1b572056fc0bb74
Guillermo Villalta <mexcelus@gmail.com>
2019-11-15 23:01:59 -0700

    footer renders

--- geoclick/src/components/Footer.jsx
+++ geoclick/src/components/Footer.jsx
@@ -1,1 +1,13 @@
 import React, { Component } from "react";
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">⚛️ - react emoji</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
