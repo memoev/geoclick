587a20b477ea3ba29fceca41ac7f6f138503ab11
Guillermo Villalta <mexcelus@gmail.com>
2019-11-15 22:26:03 -0700

    navbar component with links

--- geoclick/src/components/Nav.jsx
+++ geoclick/src/components/Nav.jsx
@@ -1,1 +1,23 @@
 import React, { Component } from "react";
+
+function Nav() {
+  return (
+    <nav class="navbar navbar-expand-lg">
+      <a class="navbar-brand" href="/">
+        Navbar
+      </a>
+      <div class="navbar" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="/">
+            Home <span class="sr-only">(current)</span>
+          </a>
+          <a class="nav-item nav-link" href="/">
+            Features
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
