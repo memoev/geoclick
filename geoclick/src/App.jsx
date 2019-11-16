b9c238ff60a72c4278ed34e744a32753451c0e8b
Guillermo Villalta <mexcelus@gmail.com>
2019-11-15 22:28:19 -0700

    added navbar

--- geoclick/src/App.jsx
+++ geoclick/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Tech from './components/Tech';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
     <>
       <header className="App-header">
+        <Nav />
         <Tech className="App"/>
       </header>
     </>
