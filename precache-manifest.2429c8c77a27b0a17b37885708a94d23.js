e41ccb79d0adcd4758dc632b2ef99530f113ef88
Guillermo Villalta <mexcelus@gmail.com>
2019-11-16 23:01:31 -0700

    Updates

---
+++ precache-manifest.2429c8c77a27b0a17b37885708a94d23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a2522808fa8d74140d3fedc4ee4345",
+    "url": "/geoclick/index.html"
+  },
+  {
+    "revision": "e4f39be0213ec947ab38",
+    "url": "/geoclick/static/css/main.36f1105d.chunk.css"
+  },
+  {
+    "revision": "4025cd89a29031980b95",
+    "url": "/geoclick/static/js/2.a6a43cb7.chunk.js"
+  },
+  {
+    "revision": "e4f39be0213ec947ab38",
+    "url": "/geoclick/static/js/main.c5d06060.chunk.js"
+  },
+  {
+    "revision": "528f1c4c6bf6ab66b2ab",
+    "url": "/geoclick/static/js/runtime-main.953ebff6.js"
+  }
+]);
