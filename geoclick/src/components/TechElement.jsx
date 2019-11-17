09ccb91212ef5b7462bb64324c28cb5b18b34084
Guillermo Villalta <mexcelus@gmail.com>
2019-11-16 21:03:59 -0700

    new props and handle click

--- geoclick/src/components/TechElement.jsx
+++ geoclick/src/components/TechElement.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-function TechElement({ key, id, src, title }) {
-    return (
-      <>
-        <img 
-            key={key} 
-            id={id} 
-            src={src} 
-            alt={title} 
-            style={{width:"100px",height:"100px"}}
+function TechElement({ key, id, src, title, handleClick }) {
+  return (
+    <>
+      <button>
+        <img
+          key={key}
+          id={id}
+          src={src}
+          alt={title}
+          style={{ width: "100px", height: "100px" }}
+          onClick={handleClick}
         />
-      </>
-    );
+      </button>
+    </>
+  );
 }
 
 export default TechElement;
